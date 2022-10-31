adead9b02e1dce4df3787db45c1707c0b100dc98
Halvor <halvor.kvamme@gmail.com>
2022-10-31 15:53:07 +0100

    Init build.rs

--- slang-sys/build.rs
+++ slang-sys/build.rs
@@ -1,16 +1,1 @@
-pub struct Release<'a> {
-    pub url: &'a str,
-}
-
-#[cfg(target_os = "linux")]
-pub static RELEASE_INFO: Release = Release {
-    url: "https://github.com/shader-slang/slang/releases/download/v0.24.38/slang-0.24.38-linux-x86_64.tar.gz",
-};
-// #[cfg(target_os = "windows")]
-// pub static RELEASE_INFO: Release = Release {
-//     url: "https://github.com/shader-slang/slang/releases/download/v0.24.38/slang-0.24.38-win64.zip",
-// };
-
 fn main() {}
-
-fn download() {}
